ec77845b455e22a7e23732c13106dd8c609f8a85
notyourphobia <notyourphobia@gmail.com>
2019-12-22 20:54:55 +0200

    Create a registration form

--- front/app/src/App.js
+++ front/app/src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import SignUp from './Components/SignUp';
+import Container from "react-bootstrap/Container";
+import Row from 'react-bootstrap/Row';
+import "./App.css";
+
 
 export default class App extends Component {
   render() {
     return (
       <div className='App'>
-        <SignUp/>
+        <Container>
+          <Row>
+            <SignUp />
+          </Row>
+        </Container>
       </div>
     );
   }
--- front/app/src/Components/SignUp.js
+++ front/app/src/Components/SignUp.js
@@ -1,33 +1,111 @@
 import React, { Component } from 'react';
+// import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 export default class SignUp extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            email: ""
+            email: '',
+            passowrd: '',
+            confirmPassowrd: '',
+            firstName: '',
+            lastName: ''
         };
         this.updateEmailField = this.updateEmailField.bind(this);
+        this.updatePasswordField = this.updatePasswordField.bind(this);
+        this.updateConfirmPasswordField = this.updateConfirmPasswordField.bind(this);
+        this.updateFirstNameField = this.updateFirstNameField.bind(this);
+        this.updateLastNameField = this.updateLastNameField.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
-
-    // updateEmailField = (event) => {
-    //     this.setState({
-    //         email: event.target.value
-    //     })
-    // }
 
     updateEmailField(event) {
         this.setState({
             email: event.target.value
         })
+
     }
 
+    updatePasswordField(event) {
+        this.setState({
+            passowrd: event.target.value
+        })
+    }
+
+    updateConfirmPasswordField(event) {
+        this.setState({
+            confirmPassowrd: event.target.value
+        })
+    }
+
+    updateFirstNameField(event) {
+        this.setState({
+            firstName: event.target.value
+        })
+    }
+
+    updateLastNameField(event) {
+        this.setState({
+            lastName: event.target.value
+        })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        console.log(JSON.stringify(this.state, 1, 1));
+    }
 
     render() {
         return (
-            <div className='sign-up'>
-                <h1>{this.state.email.length === 0 ? 'test@test.com' : this.state.email}</h1>
-                <input type="email" name="email" onChange={this.updateEmailField} />
-            </div>
+            <Col className='sign-up'>
+                {this.state.log}
+                <h1>
+                    {JSON.stringify(this.state, 1, 1)}
+                </h1>
+                <Form className='sign-up-form' onSubmit={this.handleSubmit}>
+                    <Form.Group controlId='formGroupEmail'>
+                        <Form.Label column>
+                            Email Address
+                        </Form.Label>
+                        <Form.Control type="email" name="email" onChange={this.updateEmailField} />
+                    </Form.Group>
+
+                    <Form.Group controlId='formGroupPassword'>
+                        <Form.Label column>
+                            Password
+                        </Form.Label>
+                        <Form.Control type="password" name="password" onChange={this.updatePasswordField} />
+                    </Form.Group>
+
+                    <Form.Group controlId='formGroupConfirmPassword'>
+                        <Form.Label column>
+                            Confirm Password
+                        </Form.Label>
+                        <Form.Control type="password" name="password" onChange={this.updateConfirmPasswordField} />
+                    </Form.Group>
+
+                    <Form.Group controlId='formGrouFirstName'>
+                        <Form.Label column>
+                            First Name
+                        </Form.Label>
+                        <Form.Control type="text" name="firstName" onChange={this.updateFirstNameField} />
+                    </Form.Group>
+
+                    <Form.Group controlId='formGroupLastName'>
+                        <Form.Label column>
+                            Last Name
+                        </Form.Label>
+                        <Form.Control type="text" name="lasttName" onChange={this.updateLastNameField} />
+                    </Form.Group>
+
+                    <Button variant='primary' type='submit' size='md'>
+                        Submit
+                    </Button>
+                </Form>
+            </Col>
         );
     }
 }
